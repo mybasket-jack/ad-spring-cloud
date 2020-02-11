072f167c9d6df5baf2c3c1b13eba6c4e05025dea
mybaster-jack <2354738434@qq.com>
2020-02-11 22:06:39 +0800

    广告系统骨架搭建

---
+++ ad-eureka/src/main/java/com/jack006/ad/EurekaApplication.java
@@ -0,0 +1,20 @@
+package com.jack006.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * eureka 启动程序
+ *
+ * @Author jack
+ * @Since 1.0 2020/2/11 21:03
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
